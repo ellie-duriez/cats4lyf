1e9b3352a4ac15d12477e5c10c19aeccaedbd60b
Mahed Javed <mahed95@gmail.com>
2022-07-13 15:56:16 +0100

    mahed added about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import About from './Pages/About';
+import Home from './Pages/Home';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    <h1>cats4lyf!</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>cats4lyf!</h1>
+
+        {/* link to the about page */}
+        <Link to="/home">Home</Link>
+        <Link to="/about">About Us</Link>
+
+      </div >
+
+      {/* adding routes for About page */}
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CatCard from '../CatCard';
+
+function Home() {
+    return (
+        <div>
+            <CatCard />
+        </div>
+    )
+}
+
+export default Home
