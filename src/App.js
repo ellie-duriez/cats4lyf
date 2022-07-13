4b7f996d8c361e0ac74b7d0bae8995a9cad9c253
Seb Amos <sebastian.amos8@gmail.com>
2022-07-13 17:07:54 +0100

    homepage styling

--- src/App.js
+++ src/App.js
@@ -1,54 +1,32 @@
 import './App.css';
-import About from './Pages/About';
-import Home from './Pages/Home';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import CatCard from './CatCard';
 
 function App() {
   return (
-
     <div className='App'>
-      <div className='left-page'>
-        
-        <div className='title'>cats4lyf!</div>
-        <div className='cat-container'>
-        <CatCard/><CatCard/>
-        </div>
-        <div className='cat-container'>
-        <CatCard/><CatCard/>
-        </div>
-        <div className='cat-container'>
-        <CatCard/><CatCard/>
-        </div>
+    <div className='left-page'>
+      <div className='title'>cats4lyf!</div>
+      <div className='cat-container'>
+      <CatCard/><CatCard/>
       </div>
-      <div className='right-page'>
-        <div className='basket-title'>Basket</div>
-        <div className='basket-item'>Cat1</div>
-        <div className='basket-item'>Cat2</div>
-        <div className='basket-item'>Cat3</div>
-        <div className='basket-total'>Total: £(a prop probaly)</div>
-        <br></br>
-        <div className='checkout'>Buy Now!</div>
+      <div className='cat-container'>
+      <CatCard/><CatCard/>
+      </div>
+      <div className='cat-container'>
+      <CatCard/><CatCard/>
       </div>
     </div>
-
-    <BrowserRouter>
-      <div className="App">
-        <h1>cats4lyf!</h1>
-
-        {/* link to the about page */}
-        <Link to="/home">Home</Link>
-        <Link to="/about">About Us</Link>
-
-      </div >
-
-      {/* adding routes for About page */}
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-
-  );
+    <div className='right-page'>
+      <div className='basket-title'>Basket</div>
+      <div className='basket-item'>Cat1</div>
+      <div className='basket-item'>Cat2</div>
+      <div className='basket-item'>Cat3</div>
+      <div className='basket-total'>Total: £(a prop probaly)</div>
+      <br></br>
+      <div className='checkout'>Buy Now!</div>
+    </div>
+  </div>
+);
 }
 
 export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import CatCard from '../CatCard';
 
-function Home() {
+function App() {
     return (
-        <div>
-            <CatCard />
+  
+      <div className='App'>
+        <div className='left-page'>
+          
+          <div className='title'>cats4lyf!</div>
+          <div className='cat-container'>
+          <CatCard/><CatCard/>
+          </div>
+          <div className='cat-container'>
+          <CatCard/><CatCard/>
+          </div>
+          <div className='cat-container'>
+          <CatCard/><CatCard/>
+          </div>
         </div>
-    )
-}
+        <div className='right-page'>
+          <div className='basket-title'>Basket</div>
+          <div className='basket-item'>Cat1</div>
+          <div className='basket-item'>Cat2</div>
+          <div className='basket-item'>Cat3</div>
+          <div className='basket-total'>Total: £(a prop probaly)</div>
+          <br></br>
+          <div className='checkout'>Buy Now!</div>
+        </div>
+      </div>)}
+  
 
-export default Home
+export default App
