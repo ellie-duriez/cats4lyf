c85c3984d3956149b1dfaf123a3c3914e7ec2eaf
Seb Amos <sebastian.amos8@gmail.com>
2022-07-15 15:59:39 +0100

    total

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,57 +9,30 @@ function Home() {
     const [basket, setBasket] = useState([]);
 
     const getCat = async () => {
-
-        let res = await fetch("https://api.thecatapi.com/v1/images/search?limit=20");
-
-        let data = await res.json();
-
+        let res = await fetch("https://api.thecatapi.com/v1/images/search?limit=20");let data = await res.json();
         let fakerData = Faker();
-
         let urlCopies = data.map((item) => { return item.url });
-        
-
         fakerData = fakerData.map((cat, index) => {
             cat.image = urlCopies[index];
             cat.id = index;
             return cat;
         });
-
-        // const handleSubmit = (e) => {e.preventDefault();
-        //     setItems([...items,{toDo: toDoInput, done:false},]);
-        //     setToDoInput('');};
-
-        // setCat(urlCopies);
         setCat(fakerData);
-        
     };
-    
     useEffect(() => { getCat(); }, []);
-    
     const addToBasket = (cat) => {
         let newArray = [...basket]
         newArray.push(cat)
         setBasket(newArray)
     }
     if (!cat){return <p>loading</p>};
-
     return (
         <div className="heightOfPage">
             <div className='left-page'>
-                {/* {cat ? 
-                <CatCard id={0} index={0} addToBasket={addToBasket} imgURL={cat[0].image} alt='cat' />
-                :
-                <p>loading</p>
-                
-            } */}
             {cat.map((item, index) => {
                 return (
-                <CatCard cat={item} imgURL={item.image} key={index} addToBasket={addToBasket}/>
-      );
-    })}
-                
-                
-            </div>
+                <CatCard cat={item} imgURL={item.image} key={index} addToBasket={addToBasket}/>);
+    })}</div>
             <div className='right-page'>
                 <Basket basket={basket} setBasket={setBasket}/>
             </div>
