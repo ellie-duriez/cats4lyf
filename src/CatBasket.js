dd4d0a6ae21d070c5c6279591b9ce5d2f2985f13
Seb Amos <sebastian.amos8@gmail.com>
2022-07-15 09:32:37 +0100

    kinda working basket

---
+++ src/CatBasket.js
@@ -0,0 +1,7 @@
+export const Basket =({basket, setBasket, cat})=>{
+
+    return(<form className='basket-item'>
+        <div  src={basket} alt='catbuy'>
+        </div>
+    </form>)
+}
--- src/CatCard.js
+++ src/CatCard.js
@@ -1,54 +1,19 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import { Button } from "./CatCardCss";
 import Faker from "./Faker";
 import './App.css';
 
-const CatCard = () => {
-    const [cat, setCat] = useState("");
-    const getCat = async () => {
-        let res = await fetch("https://api.thecatapi.com/v1/images/search");
-        let data = await res.json();
-        let urlCopies = data.map((item, index) => {return item.url;});
-        setCat(urlCopies);};
-        useEffect(() => {getCat();}, []);
+const CatCard = ({addToBasket, cat, setCat}) => {
+
         return (<div className='cat-container' >
-        <Button >
-            <img src={cat[0]} alt="cat"></img>
+        <Button onClick={addToBasket}>
+            <img ></img>
             <Faker />
         </Button>
         </div>);
         };
 
-      export const Basket =({cat})=>{
-            const [basket, setBasket] = useState([]);
-            const addToBasket =()=>{setBasket(cat)
-        
-            }
-            return(<form className='basket-item'>
-                <div  src={basket} alt='catbuy'>
-                </div>
-            </form>)
-        }
+
 
 export default CatCard;
 
-// function App() {
-//   const [cookie, setCookie] = useState('');
-//   const getData = async () =>{
-//     let res = await fetch('https://api.adviceslip.com/advice');
-//     let data = await res.json();
-//     setCookie(data.slip);
-//   };
-//   useEffect(()=>{getData()},[]);
-//   return (
-//     <div className="App">
-//       <h1>Fortune Cookie</h1>
-//       {cookie.advice ?
-//       <h2>{cookie.advice}</h2>
-//     :
-//     <h2>no cookie</h2>}
-//       <button onClick={getData}>open cookie</button>
-//     </div>
-//   );
-// }
