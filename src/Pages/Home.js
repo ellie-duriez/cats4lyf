d074dd2cb36b08576e7b2b126a4a84c89c48ffbc
Mahed Javed <mahed95@gmail.com>
2022-07-15 09:55:28 +0100

    [mahed] make cat images appear

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import Basket from '../CatCard';
 import CatCard from '../CatCard';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 function Home() {
     const [cat, setCat] = useState("");
     const getCat = async () => {
         let res = await fetch("https://api.thecatapi.com/v1/images/search");
         let data = await res.json();
-        let urlCopies = data.map((item, index) => {return item.url;});
-        setCat(urlCopies);};
-        useEffect(() => {getCat();}, []);
+        let urlCopies = data.map((item, index) => { return item.url; });
+        setCat(urlCopies);
+    };
+    useEffect(() => { getCat(); }, []);
     const [basket, setBasket] = useState([]);
-    const addToBasket =()=>{
-        setBasket([...cat],{name:0})
-        }
+    const addToBasket = () => {
+        setBasket([...cat], { name: 0 })
+    }
 
     return (
         <div className="heightOfPage">
             <div className='left-page'>
-                    <CatCard addToBasket={addToBasket} src={cat[0]} alt='cat'/><CatCard />
-                    <CatCard /><CatCard />
-                    <CatCard /><CatCard />
+                <CatCard addToBasket={addToBasket} alt='cat' imgURL={cat} /><CatCard imgURL={cat} />
+                <CatCard imgURL={cat} /><CatCard imgURL={cat} />
+                <CatCard imgURL={cat} /><CatCard imgURL={cat} />
             </div>
             <div className='right-page'>
-                <Basket value={basket}/>
+                <Basket value={basket} />
             </div>
         </div>
     )
